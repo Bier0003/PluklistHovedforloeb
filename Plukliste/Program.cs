403f2da88c9a928f992fcd7ed47652233c3c6cef
Pornpan Madsen <bier0003@study.sde.dk>
2023-09-26 07:14:02 +0200

    Implemented story 1

--- Plukliste/Program.cs
+++ Plukliste/Program.cs
@@ -1,27 +1,27 @@
-﻿namespace Plukliste;
+﻿using System.Text;
+
+namespace Plukliste;
 
 class PluklisteProgram { 
-
+    
     static void Main()
     {
         //Arrange
         char readKey = ' ';
         List<string> files;
+        //List<string> templateFiles;
         var index = -1;
         var standardColor = Console.ForegroundColor;
         Directory.CreateDirectory("import");
-
-        if (!Directory.Exists("export"))
-        {
-            Console.WriteLine("Directory \"export\" not found");
-            Console.ReadLine();
-            return;
-        }
-        files = Directory.EnumerateFiles("export").ToList();
+        Directory.CreateDirectory("print");
+                
+        files = readFilesFromDirectory("export");
+        //templateFiles = readFilesFromDirectory("templates");
 
         //ACT
         while (readKey != 'Q')
         {
+            Pluklist plukliste = null;
             if (files.Count == 0)
             {
                 Console.WriteLine("No files found.");
@@ -39,19 +39,19 @@ class PluklisteProgram {
                 {
                     System.Xml.Serialization.XmlSerializer xmlSerializer =
                         new System.Xml.Serialization.XmlSerializer(typeof(Pluklist));
-                    var plukliste = (Pluklist?)xmlSerializer.Deserialize(file);
+                    plukliste = (Pluklist?)xmlSerializer.Deserialize(file);
 
                     //print plukliste
                     if (plukliste != null && plukliste.Lines != null)
                     {
-                        Console.WriteLine("\n{0, -13}{1}", "Name:", plukliste.Name);
-                        Console.WriteLine("{0, -13}{1}", "Forsendelse:", plukliste.Forsendelse);
-                        Console.WriteLine("{0, -13}{1}", "Addresse:", plukliste.Adresse);
+                        Console.WriteLine($"\n{"Name:",-13}{plukliste.Name}");
+                        Console.WriteLine($"{"Forsendelse:",-13}{plukliste.Forsendelse}");
+                        Console.WriteLine($"{"Addresse:",-13}{plukliste.Adresse}");
 
-                        Console.WriteLine("\n{0,-7}{1,-9}{2,-20}{3}", "Antal", "Type", "Produktnr.", "Navn");
+                        Console.WriteLine(GetPluklisteHeadline());
                         foreach (var item in plukliste.Lines)
                         {
-                            Console.WriteLine("{0,-7}{1,-9}{2,-20}{3}", item.Amount, item.Type, item.ProductID, item.Title);
+                            Console.WriteLine(GetPluklisteLine(item));
                         }
                     }
                 }
@@ -59,35 +59,28 @@ class PluklisteProgram {
 
             //Print options
             Console.WriteLine("\n\nOptions:");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("Q");
-            Console.ForegroundColor = standardColor;
-            Console.WriteLine("uit");
+            PrintOption("Q", "uit", standardColor);
+
             if (index >= 0)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("A");
-                Console.ForegroundColor = standardColor;
-                Console.WriteLine("fslut plukseddel");
+                PrintOption("A", "fslut plukseddel", standardColor);
             }
             if (index > 0)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("F");
-                Console.ForegroundColor = standardColor;
-                Console.WriteLine("orrige plukseddel");
+                PrintOption("F", "orrige plukseddel", standardColor);
             }
             if (index < files.Count - 1)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("N");
-                Console.ForegroundColor = standardColor;
-                Console.WriteLine("æste plukseddel");
+                PrintOption("N", "æste plukseddel", standardColor);
             }
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("G");
-            Console.ForegroundColor = standardColor;
-            Console.WriteLine("enindlæs pluksedler");
+
+            PrintOption("G", "enindlæs pluksedler", standardColor);
+
+            PrintOption("O", "pgraderingsvejledning udskrift", standardColor);
+
+            PrintOption("U", "psigelsesvejledning udskrift", standardColor);
+
+            PrintOption("V", "elkomst udskrift", standardColor);
 
             readKey = Console.ReadKey().KeyChar;
             if (readKey >= 'a') readKey -= (char)('a' - 'A'); //HACK: To upper
@@ -115,9 +108,70 @@ class PluklisteProgram {
                     files.Remove(files[index]);
                     if (index == files.Count) index--;
                     break;
+                case 'O': //Opgradeingsvejledning
+                    PrintManual("OPGRADE", plukliste, true);
+                    Console.WriteLine("Opgraderingsvejledning sendt til print!");
+                    break;
+                case 'U': //Opsigelsesvejledning
+                    PrintManual("OPSIGELSE", plukliste, false);
+                    Console.WriteLine("Opsigelsesvejledning sendt til print!");
+                    break;
+                case 'V': //Velkomst vejledning
+                    PrintManual("WELCOME", plukliste, true);
+                    Console.WriteLine("Velkomst sendt til print!");
+                    break;
             }
             Console.ForegroundColor = standardColor; //reset color
 
         }
+    }
+
+    private static string GetPluklisteLine(Item item)
+    {
+        return $"{item.Amount,-7}{item.Type,-9}{item.ProductID,-20}{item.Title}";
+    }
+
+
+    private static string GetPluklisteHeadline()
+    {
+        return $"\n{"Antal",-7}{"Type",-9}{"Produktnr.",-20}{"Navn"}";
+    }
+
+
+    public static void PrintManual(string type, Pluklist plukliste, bool printPlukliste)
+    {
+        var content = File.ReadAllText("templates/PRINT-" + type + ".html");
+        content = content.Replace("[Adresse]", plukliste.Adresse);
+        content = content.Replace("[Name]", plukliste.Name);
+        if(printPlukliste)
+        {
+            var sb = new StringBuilder();
+            sb.Append("<pre>");
+            sb.AppendLine(GetPluklisteHeadline());
+            foreach(var item in plukliste.Lines)
+                sb.AppendLine(GetPluklisteLine(item));
+            sb.Append("</pre>");
+            content = content.Replace("[Plukliste]", sb.ToString());
+        }
+        File.WriteAllText("print/" + Guid.NewGuid().ToString() + ".html", content);
+    }
+
+    public static void PrintOption(string optionLetter, string followupText, ConsoleColor foregroundColor)
+    {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(optionLetter);
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine(followupText);
+    }
+
+    public static List<string> readFilesFromDirectory(string directoryName)
+    {
+        if (!Directory.Exists(directoryName))
+        {
+            Console.WriteLine($"Directory \"{directoryName}\" not found");
+            Console.ReadLine();
+            return null;
+        }
+        return Directory.EnumerateFiles(directoryName).ToList();;
     }
 }
